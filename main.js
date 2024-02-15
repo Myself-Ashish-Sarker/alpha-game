11cd132e5863f78e334fc81be8fb4008c84fbd3b
Ashish Sarker <myselfashishsarker@gmail.com>
2024-02-15 20:23:36 +0600

    clicking the 'play now' button to hide the entire section

---
+++ main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // DOM declaration
+    const sectionOne = document.querySelector(".section-one")
+
+    const btnOne = document.querySelector(".btn-one")
+    // DOM declaration
+
+    btnOne.addEventListener("click", () => {
+        console.log("OK!")
+        sectionOne.classList.add("hide-one")
+    })
+})
