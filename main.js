834a24ddaaf66045b577a412da6036f2779344eb
Ashish Sarker <myselfashishsarker@gmail.com>
2024-02-16 13:04:49 +0600

    random alphabets will generate in a screen

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const sectionOne = document.querySelector(".section-one");
 const sectionTwo = document.querySelector(".section-two")
 const sectionThree = document.querySelector('.section-three');
 
+const randomSign = document.querySelector('.random-alphabets');
+
 const btnOne = document.querySelector(".btn-one")
 const btnThree = document.querySelector('.btn-three')
 // DOM declaration
@@ -12,6 +14,7 @@ btnOne.addEventListener('click', () => {
     sectionOne.style.display = 'none';
     sectionTwo.style.display = 'block';
     sectionThree.style.display = 'none';
+    continueGame();
 })
 
 btnThree.addEventListener('click', () => {
@@ -20,4 +23,21 @@ btnThree.addEventListener('click', () => {
     sectionTwo.style.display = 'block';
 })
 // adding addEventListener
+
+// random number generator
+function generateRandomAlphabet() {
+    const allAlphabets = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = allAlphabets.split('');
+    const randomNumber = Math.random() * 25;
+    const index = Math.round(randomNumber);
+    const alphabet = alphabets[index];
+    return alphabet;
+}
+// random number generator
+
+function continueGame() {
+    const alphabet = generateRandomAlphabet();
+    randomSign.innerText = alphabet;
+}
+
 
